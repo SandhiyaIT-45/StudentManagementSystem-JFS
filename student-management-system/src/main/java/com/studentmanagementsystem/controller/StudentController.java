3370ee8e01db9e5cb61436e6d1881905f35394cd
Sandhiya J <santhiyaj439@gmail.com>
2026-07-09 21:37:56 +0530

    initial spring boot stdmngsys

---
+++ student-management-system/src/main/java/com/studentmanagementsystem/controller/StudentController.java
@@ -0,0 +1,32 @@
+package com.studentmanagementsystem.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import com.studentmanagementsystem.model.Student;
+import com.studentmanagementsystem.service.StudentService;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return studentService.addStudent(student);
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentService.getStudentById(id);
+    }
+}
---
+++ student-management-system/src/main/java/com/studentmanagementsystem/service/StudentService.java
@@ -0,0 +1,23 @@
+package com.studentmanagementsystem.service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.studentmanagementsystem.model.Student;
+import com.studentmanagementsystem.repository.StudentRepository;
+@Service
+public class StudentService {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public Student addStudent(Student student) {
+        return studentRepository.save(student);
+    }
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    public Student getStudentById(int id){
+        return studentRepository.findById(id).orElse(null);
+    }
+}
